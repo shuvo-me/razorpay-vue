b9b338b75338fe0971ee56779377ca29c458a9ef
shuvo-me <shuvonaim123@gmail.com>
2022-08-09 12:13:31 +0600

    uploading project

---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<!-- <script src="https://checkout.razorpay.com/v1/checkout.js"></script> -->
+<script setup>
+var options = {
+  key: "rzp_test", // Enter the Key ID generated from the Dashboard
+  amount: "1000000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+  currency: "INR",
+  name: "Acme Corp",
+  description: "Test Transaction",
+  image: "https://example.com/your_logo",
+  order_id: "order_K3QVPyGWQiLJRW", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+  // callback_url: "https://eneqd3r9zrjok.x.pipedream.net/",
+  prefill: {
+    name: "Gaurav Kumar",
+    email: "gaurav.kumar@example.com",
+    contact: "9999999999",
+  },
+  notes: {
+    address: "Razorpay Corporate Office",
+  },
+  theme: {
+    color: "#3399cc",
+  },
+};
+const rzp1 = new Razorpay(options);
+
+rzp1.on("payment.success", function (response) {
+  console.log(response);
+});
+
+rzp1.on("payment.failed", function (response) {
+  console.log(response);
+});
+const handlePay = (e) => {
+  rzp1.open();
+};
+</script>
+
+<template>
+  <!-- <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView /> -->
+  <main>
+    <button @click="handlePay">Pay With Razorpay</button>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
